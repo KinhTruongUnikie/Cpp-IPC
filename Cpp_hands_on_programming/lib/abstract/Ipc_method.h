b6091f3f81da2041f98cc0ce740ae9f965d64cec
Kinh Truong <kinh.truong@unikie.com>
2022-05-16 02:21:29 -0700

    minor fix

--- Cpp_hands_on_programming/lib/abstract/Ipc_method.h
+++ Cpp_hands_on_programming/lib/abstract/Ipc_method.h
@@ -12,9 +12,7 @@ public:
     virtual void send() = 0;
     virtual void receive() = 0;
     ssize_t getFileSize(const std::string &filename);
-    //int readFile(const std::string &filename, int offset, std::vector<char> &buffer, int size);
     int readFile(std::ifstream &in, const std::string &filename, std::vector<char> &buffer, int size);
-    //int writeFile(const std::string &filename, int total, const std::vector<char> &buffer);
     int writeFile(std::ofstream &out, const std::string &filename, const std::vector<char> &buffer);
     void clearFile(std::string filename);
     std::string getFileName_absolute(const std::string &filename);
